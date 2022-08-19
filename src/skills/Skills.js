8ff354aba7338e853fed8e905085c3288ff7131c
LaVon <leonidmedica@gmail.com>
2022-08-19 13:25:49 +0200

    skills finished

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,17 +3,39 @@ import style from "./Skills.module.css";
 import styleContainer from "./../common/styles/Container.module.css"
 import {Skill} from "./skill/Skill";
 import {Title} from "../common/components/title/Title";
+import htmlIcon from "../assets/icon/html-svgrepo-com.svg"
+import jsIcon from "../assets/icon/jsx-svgrepo-com.svg"
+import cssIcon from "../assets/icon/css-svgrepo-com.svg"
+import reactIcon from "../assets/icon/react-svgrepo-com.svg"
+import tsIcon from "../assets/icon/typescript-svgrepo-com.svg"
 
 
 export const Skills = () => {
     return (
         <div className={style.skillsBlock}>
             < div className={`${styleContainer.container} ${style.skillsContainer}`}>
-                <Title title={'Skills'} description = {'DEVELOPING ON'}/>
+                <Title title={'Skills'} description={'DEVELOPING ON'}/>
                 <div className={style.skills}>
-                   <Skill title="JS" description ="11111111 1111 1111111 11111111 1111111 1111111111111111 1111 11111 11111111 11111 1111 "/>
-                   <Skill title="HTML" description="2222222222 22222222 2222222 222222 2 2 222222222 222  222  2222  2222222  2 2 2222"/>
-                   <Skill title="CSS" description="33333333 33333 3 3 33333333 3 333333 3333 333333333 333 33333333 333333333 3333333 33333"/>
+                    <Skill title="JS"
+                           description="sometimes I press a button and some miracle happens"
+                           icon={jsIcon}
+                    />
+                    <Skill title="HTML"
+                           description="words come step by step"
+                           icon={htmlIcon}
+                    />
+                    <Skill title="CSS"
+                           description="big pictures don't run away anymore"
+                           icon={cssIcon}
+                    />
+                    <Skill title="React"
+                           description="many smart things in one place"
+                           icon={reactIcon}
+                    />
+                    <Skill title="TypeScript"
+                           description="in this js code you wont make mistakes. DON'T USE ':any'"
+                           icon={tsIcon}
+                    />
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -2,15 +2,14 @@ import React from 'react';
 import style from "./Skill.module.scss";
 
 
-
 export const Skill = (props) => {
     return (
         <div className={style.skillBlock}>
-               <div className={style.icon}>+</div>
-                <h3 className={style.title}>{props.title}</h3>
-                <div className={style.description}>
-                    <span>{props.description}</span>
-                </div>
+            <img className={style.icon} src={props.icon} alt={"0"}/>
+            <h3 className={style.title}>{props.title}</h3>
+            <div className={style.description}>
+                <span>{props.description}</span>
+            </div>
         </div>
     );
 }
