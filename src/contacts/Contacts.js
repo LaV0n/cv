f3257b7e80c6aa489b65619bb2598b0418db18a6
LaVon <leonidmedica@gmail.com>
2022-08-20 13:39:09 +0200

    end with contacts

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -2,22 +2,85 @@ import React from 'react';
 import style from "./Contacts.module.scss";
 import styleContainer from "./../common/styles/Container.module.css"
 import {Title} from "../common/components/title/Title";
+import {createMuiTheme,  ThemeProvider} from '@mui/material/styles';
+import {Button, styled, TextField} from "@mui/material";
 
+const theme = createMuiTheme({
+    status: {
+        danger: 'primary',
+    },
+    palette: {
+        button: {
+            main: '#00ddce',
+        },
+        personal: {
+            main: '#00ddce',
+        },
+        typography: {
+            fontSize: '100',
+            fontFamily: 'Poppins',
+        },
+        text: {
+            primary: '#fff',
+            secondary: 'rgba(253,253,253,0.4)'
+        },
+
+    }
+});
+const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+        color: '#00ddce',
+    },
+    '& .MuiInput-underline:after': {
+        borderBottomColor: '#00ddce',
+    },
+    '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+            borderColor: 'rgba(253,253,253,0.4)',
+        },
+        '&:hover fieldset': {
+            borderColor: 'fff',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: '#00ddce',
+        },
+    },
+});
 
 export const Contacts = () => {
     return (
-        <div className={style.contactsBlock}>
-            < div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                <Title title={'Contacts'} description ={'LET\'S TALK'}/>
-                <div className={style.block}>
-                    <form className={style.form}>
-                        <input placeholder="<input>"/>
-                        <input placeholder="<input>"/>
-                        <textarea placeholder="<textarea>"/>
-                    </form>
-                    <button type='submit'>send</button>
+        <ThemeProvider theme={theme}>
+            <div className={style.contactsBlock}>
+                < div className={`${styleContainer.container} ${style.contactsContainer}`}>
+                    <Title title={'Contacts'} description={'LET\'S TALK'}/>
+                    <div className={style.block}>
+                        <form className={style.form}>
+                            <CssTextField id="nameInput"
+                                       label="FULL NAME *"
+                                       variant="outlined"
+                                       margin="normal"
+                            />
+                            <CssTextField id="mailInput"
+                                       label="EMAIL ADDRESS *"
+                                       variant="outlined"
+                                       margin="normal"
+                            />
+                            <CssTextField id="massageInput"
+                                          label="MESSAGE *"
+                                          variant="outlined"
+                                          multiline
+                                          rows={4}
+                                          margin="normal"
+                                          />
+                        </form>
+                        <Button type='submit'
+                                variant="outlined"
+                                color="button"
+                                size="large"
+                        >send</Button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </ThemeProvider>
     );
 }
