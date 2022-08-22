cdde27a061a6f7129fc06a7af517c901b49ec131
LaVon <leonidmedica@gmail.com>
2022-08-22 20:19:48 +0200

    burger menu

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,52 +1,71 @@
 import React from 'react';
 import style from "./Navbar.module.scss";
-import {
-    Accordion,
-    AccordionDetails,
-    AccordionSummary,
-    Typography
-} from "@mui/material";
+import {  IconButton, Menu, MenuItem} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+const options = [
+    <a className={style.link} href="">main </a>,
+    <a className={style.link} href="">skills</a>,
+    <a className={style.link} href="">projects</a>,
+    <a className={style.link} href="">contacts</a>
+];
+
+const ITEM_HEIGHT = 48;
 
 export const Navbar = () => {
 
-    const [expanded, setExpanded] = React.useState(false);
-
-    const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
     };
 
 
-
     return (<>
-
-            {/*<div className={style.nav}>
+            <div className={style.nav}>
                 <a href="">main </a>
                 <a href="">skills</a>
                 <a href="">projects</a>
                 <a href="">contacts</a>
-            </div>*/}
-
-                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-                    <AccordionSummary
-                        aria-controls="panel1bh-content"
-                        id="panel1bh-header"
-                    >
-                        <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                          MENU
-                        </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            <div className={style.nav}>
-                            <a href="">main </a>
-                            <a href="">skills</a>
-                            <a href="">projects</a>
-                            <a href="">contacts</a>
-                            </div>
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-
+            </div>
+            <div className={style.burgerMenu}>
+                <IconButton
+                    aria-label="more"
+                    id="long-button"
+                    aria-controls={open ? 'long-menu' : undefined}
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                    color={"inherit"}
+                >
+                 <MenuIcon/>
+                </IconButton>
+                <Menu
+                    id="long-menu"
+                    MenuListProps={{
+                        'aria-labelledby': 'long-button',
+                    }}
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                        style: {
+                            width: '100%',
+                            backgroundColor:'#373C40FF'
+                        },
+                    }}
+                >
+                    {options.map((option) => (
+                        <MenuItem key={option}
+                                  onClick={handleClose}>
+                            {option}
+                        </MenuItem>
+                    ))}
+                </Menu>
+            </div>
         </>
     );
 }
