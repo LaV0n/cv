312f2268daaa83228fd6e67082d1e51e7b19522b
LaVon <leonidmedica@gmail.com>
2022-09-12 11:12:04 +0200

    add photo hover

--- src/main/Main.js
+++ src/main/Main.js
@@ -11,7 +11,7 @@ export const Main = () => {
         <div id='#main' className={style.main}>
             < div className={`${styleContainer.container} ${style.mainContainer}`}>
                 <Slide bottom>
-                    <img src={photo} className={style.photo} alt='0'></img>
+                    <img src={photo} className={style.photo} alt='0'/>
                     <div className={style.greetings}>
                         <h1 className={style.name}>
                     <span>Leanid
@@ -22,7 +22,7 @@ export const Main = () => {
                                 <ReactTypingEffect text={['FRONTEND DEVELOPER']}/>
                                 </span>
                             <p>I'm an ambitious and motivated
-                                Frontend  Developer. I'm ready to
+                                Frontend Developer. I'm ready to
                                 work in a team but don't afraid of
                                 looking for the solutions on my own.
                                 I would like to work in a dynamic
